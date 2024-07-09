e0dcf080f946f971a3f48263ec2c256211fb7ae0
JLegacy-AI <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-07-09 23:09:02 +0500

    class1

---
+++ lecture-1/class1.js
@@ -0,0 +1,80 @@
+/*
+
+Programming Language
+
+syntax: How to write
+
+variable: 
+
+value: 
+Datatypes: Types
+integer: 0,1,2,3,4 / Float: 0.12, 0.23,19.23 => Number
+string: "" / '' : "My Name is Jamal" / 'Hello' / '123'
+ 
+function
+*/
+
+// let acb_asd = 10;
+// var y = 10;
+
+// console.log(acb_asd);
+
+// keyword identifier_ = value;
+// keyword: let, var, const
+// value: Number/String
+// identifer_: 12
+//
+// Data Type: value
+// console.log(typeof 10.0);
+// console.log(typeof "12");
+
+// console.log(typeof 12);
+
+// const: Once declare, nor redeclare or reassign
+
+// Reassign
+// const age = 12;
+// age = 123; // Reassign
+// console.log(age);
+
+// const AGE = 19; // Declaration / Declare
+// const AGE = 20; // Redeclare
+// console.log(AGE)
+
+// let:  Once declare, can be Reassign but not redeclare
+
+// let x = "abc";
+// x = "def";
+// console.log(x);
+
+// let x = "abc";
+// let x = "def";
+// console.log(x);
+
+// var
+
+// Redeclare
+// var x = 10;
+// var x = 12;
+// var x = "Hi";
+// console.log(x);
+
+//Reassign
+// var x = 10;
+// x = "Jamal";
+// console.log(x);
+
+// Declaration
+// <keyword> identifer; => let / var
+
+// Initialization
+// <keyword> identifer = value;
+
+// Assignment
+// identifer = value;
+
+// const x;
+
+// let x;
+
+//  console.log(identifer/value)
