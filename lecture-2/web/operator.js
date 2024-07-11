fa8468818d9cdd09b52483537e7f0c56acd84477
JLegacy-AI <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-07-11 23:04:22 +0500

    Lecture-2

---
+++ lecture-2/web/operator.js
@@ -0,0 +1,143 @@
+/*
+
+-> Logical Operators: 1 / 0 | True / False :  
+
+    boolean
+
+    and: &&
+    or: ||
+
+    not: !
+
+    equal to: ==  ===
+    greater than: >
+    less than: <
+
+    greater than equal to: >=
+    less than equal to: <=
+
+    Syntax: <> op <>
+
+    Syntax: <> && <>
+        Both side should be true
+        true && true : 1 & 1  = true
+        true && false : 1 & 0 = false
+
+    Syntax: <> || <>
+        Either should be true then True / 1
+        true || false : false || true  =  1 / True
+        false || false = 0 / false
+
+    Syntax: ! <>
+        !true = false
+        !false = true
+
+-> Mathematical Operator: + - / * %:
+    Syntax: <> op <>
+    +   ,  1 + 1 = 2
+    -   ,  1 - 1 = 0
+    /   ,  4 / 2 = 2
+    *   ,  4 * 2 = 8
+    %   ,  4 % 2 = 0,  3 % 2 = 1,  9 % 5 = 4
+
+-> Bitwise Operator:
+
+    result: number
+
+    and: &
+    or: |
+
+    Syntax: <> & <> 
+        Both side should be true
+        true & true : 1 & 1  = true
+        true & false : 1 & 0 = false
+    
+    Syntax: <> | <>
+        Either should be true then True / 1
+        true | false : false | true  =  1 / True
+        false | false = 0 / false
+
+-> 
+
+Datatype: Boolean = True / False
+
+*/
+
+// const x = true;
+// const y = false;
+
+// console.log(x & y);
+// console.log("true & false: ", true & false);
+// console.log("true & true: ", true & true);
+
+// OR Operator
+
+// console.log("true | true = ", true | true);
+// console.log("false | true = ", false | true);
+// console.log("true | false = ", true | false);
+// console.log("false | false = ", false | false);
+
+// Not Operator
+
+// console.log("! true", !true);
+// console.log("! false", !false);
+
+// && Operator
+// console.log("true && false: ", true && false);
+// console.log("true && true: ", true && true);
+
+// OR Operator
+
+// console.log("true || true = ", true || true);
+// console.log("false || true = ", false || true);
+// console.log("true || false = ", true || false);
+// console.log("false || false = ", false || false);
+
+// const x = true;
+// const y = false;
+// const result = x && y;
+// console.log("Result ", result);
+
+/*
+
+    Mathematical Operators
+
+*/
+
+// const x = 10;
+// const y = 20;
+
+// console.log("Add: ", x + y);
+// console.log("Sub: ", x - y);
+// console.log("Mul: ", x * y);
+// console.log("Div: ", x / y);
+// console.log("Mod: ", x % y);
+
+// const x = 10;
+// const y = 20;
+
+// const add = x + y;
+// const sub = x - y;
+// const mul = x * y;
+// const div = x / y;
+// const mod = x % y;
+
+// console.log("Add: ", add);
+// console.log("Sub: ", sub);
+// console.log("Mul: ", mul);
+// console.log("Div: ", div);
+// console.log("Mod: ", mod);
+
+// console.log(typeof (true & true));
+// console.log(typeof (true && true));
+
+// console.log(4 + (1 + 2) * 0 * (77172 * 4));
+
+// console.log(1 + 2 + false);
+
+/**
+ *
+ *
+ *
+ *
+ */
